ef2a124b8d4b71b7148e04d3724a2fa95b8613ad
Sunse Kwon <sunse523@gmail.com>
2024-03-16 17:34:36 +0900

    stock price prediction using mlflow

--- run.py
+++ run.py
@@ -3,7 +3,10 @@ import mlflow
 experiment_name = "XGBRegressor"
 entry_point = "Training"
 
-mlflow.set_tracking_uri("http://ec2-13-209-26-166.ap-northeast-2.compute.amazonaws.com:5000/")
+mlflow.set_tracking_uri(
+    # "http://ec2-13-209-26-166.ap-northeast-2.compute.amazonaws.com:5000/"
+    'http://127.0.0.1:5000/'
+)
 # import pdb; pdb.set_trace()
 mlflow.projects.run(
 
